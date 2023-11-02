dee4991e5e61c965de9960f39071fc5f44f35b52
Felipe Oliveira <138164801+felipeoliveirasites@users.noreply.github.com>
2023-11-02 00:51:40 -0300

    feito

--- aula12/ex013.js
+++ aula12/ex013.js
@@ -10,7 +10,37 @@ var diaSem = agora.getDay()
 5 = Sexta
 6 = Sábado
 */
-console.log(diaSem)
-if (diaSem == 0){
+//console.log(diaSem)
+/*if (diaSem == 0){
     console.log('Domingo')
+} */
+
+switch(diaSem) {
+    case 0: 
+        console.log('Domingo')
+        
+    case 1: 
+        console.log('Segunda')
+        
+    case 2: 
+        console.log('Terça')
+        
+    case 3:
+        console.log('Quarta')
+        
+    case 4:
+        console.log('Quinta')
+        
+    case 5:
+        console.log('Sexta')
+        
+    case 6:
+        console.log('Sábado')
+        
+    default:
+        console.log('[ERRO] Dia invalido!')
+        
 }
+
+//entre 0 e 8 use if
+//se for dados pontuais (exatos) use o switch só funciona com números inteiros e caracteres (strings) mas limitada do que o if mas útil para situações pontuais escreve menos
