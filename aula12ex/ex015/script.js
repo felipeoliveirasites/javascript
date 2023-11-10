9230625951ba5491a92e72cb9e45ee2e18eec75d
Felipe Oliveira <138164801+felipeoliveirasites@users.noreply.github.com>
2023-11-10 07:08:49 -0300

    andamento

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,1 +1,24 @@
-alert('Olá!')
+//alert('Olá!')
+function verificar() {
+    //window.alert("FUNCIONOU!")
+    var data = new Date()
+    var ano = data.getFullYear()
+    var meuano = document.getElementById('numano')
+    var res = document.querySelector('div#res')
+    if (meuano.value.length == 0 || meuano.value > ano) {
+      //(meuano.value.length == 0 || Number(meuano.value > ano)) 
+        window.alert("Verifique os dados e tente novamente!")
+    } else {
+       var radsex = document.getElementsByName("radsex")
+       var idade = ano - meuano.value
+       res.innerHTML = `Idade calculada: ${idade} anos`
+       var gênero = ``
+       if (radsex[0].checked) {
+        gênero = "Homem"
+       } else if(radsex[1].checked) {
+            gênero = "Mulher"
+        }
+      res.innerHTML = `Detectamos ${gênero} com ${idade} anos`
+      res.innerHTML.st
+       }
+    }
