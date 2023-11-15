2ad11772933f3fce06104a2d8924e5ca77dbb92c
Felipe Oliveira <138164801+felipeoliveirasites@users.noreply.github.com>
2023-11-15 20:16:12 -0300

    atualizar

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -11,16 +11,50 @@ function verificar() {
     } else {
        var radsex = document.getElementsByName("radsex")
        var idade = ano - meuano.value
-       res.innerHTML = `Idade calculada: ${idade} anos`
+       //res.innerHTML = `Idade calculada: ${idade} anos`
        var gênero = ``
+       var img = document.createElement("img")
+       var section = document.querySelector('section')
+       img.setAttribute('id', 'foto')
+       img.style.marginTop = '10px'
        if (radsex[0].checked) {
         gênero = "Homem"
+        if (idade >= 0 && idade < 10) {
+          //Criança
+          img.setAttribute('src', 'imagens/foto-crianca-m.png')
+        } else if (/*idade >= 10 && */idade < 40) {
+          //Jovem
+          img.setAttribute('src', 'imagens/foto-jovem-m.png')
+        } else if ( idade < 65) {
+          //Adulto
+          img.setAttribute('src', 'imagens/foto-adulto-m.png')
+        } else { 
+          //Idoso
+          img.setAttribute('src', 'imagens/foto-idoso-m.png')
+        }
        } else if(radsex[1].checked) {
             gênero = "Mulher"
+            if (idade >= 0 && idade < 10) {
+              //Criança
+              img.setAttribute('src', 'imagens/foto-crianca-f.png')
+            } else if (/*idade >= 10 && */idade < 40) {
+              //Jovem
+              img.setAttribute('src', 'imagens/foto-jovem-f.png')
+            } else if ( idade < 65) {
+              //Adulto
+              img.setAttribute('src', 'imagens/foto-adulto-f.png')
+            } else { 
+              //Idoso
+              img.setAttribute('src', 'imagens/foto-idoso-f.png')
+            }
         }
         res.innerHTML = `Detectamos ${gênero} com ${idade} anos`
         res.style.textAlign = 'center'
+        res.appendChild(img)
+        section.style.background = "#e0a1a1"
        }
     }
+
+    
 
     
---
+++ aula13/ambiente.js
@@ -0,0 +1,36 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+} while (c <= 5)
+
+/* var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+} /*
+
+
+//while estrutura de repetição com teste lógico de início
+//do..while estrutura de repetição com teste lógico no  final
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
+ 
+/* Estrutura de repetição com variável de controle */ 
+//for(inicio; teste; incr) { } posso colocar dentro de um for um while um do..while, um if, um switch eu posso colocar qulque estrutura que a gente viu das aulas anteriores até aqui
+
+/* var = 1
+while(c<=10){
+
+    c++
+} */
+for (var d = 1;d<=10;d++) {
+console.log(`Etapa ${d}`)
+}
