e27c93aaed02a3f9bf67dd7b2660e25c04ac2388
Felipe Oliveira <138164801+felipeoliveirasites@users.noreply.github.com>
2023-11-20 09:56:14 -0300

    Update ambiente.js

--- aula14/ambiente.js
+++ aula14/ambiente.js
@@ -1,5 +1,17 @@
-c = 1
-while (c <= 10) {
-    console.log(`c`)
+/*var c = 1
+while (c <= 5) {
+    console.log(c)
     c++
-}
+} */ /*
+var d = 1
+do {
+    console.log(d)
+    d++
+} while (d<= 6) */
+console.log('Vai começar...')
+for (var d=1; d<=4; d++) {
+    console.log(d)
+} 
+console.log('FIM!')
+
+/*pode esta funcionando sem erro de sintaxe mas com erro de lógica e não vai dar erro aqui no meu console */
